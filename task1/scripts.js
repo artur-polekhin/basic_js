4ac7dac478ef4fa7e6d23d5a01eb5f9aa233de92
Ko4aH4uK <>
2025-11-30 20:24:47 +0200

    Modul 1

--- task1/scripts.js
+++ task1/scripts.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+// Task 1
+console.log("Hello, Ko4aH4uK");
+console.log("You're to old the say your name out loud");
+
+// Task 2
+const name = "John";
+
+alert(`Hello ${name}`);
+
+// Task 3
+
+console.log("Name: John");
+console.log("Age: 40");
+console.log("Town: New-York");
+console.log("Profession: Unemployed");
+console.log("Goal: General QA");
